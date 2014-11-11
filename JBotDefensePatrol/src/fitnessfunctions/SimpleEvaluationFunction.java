f354cf4b4b8b223a1f20f2bddd8d7613f09e603e
diogorbperes@gmail.com <diogorbperes@gmail.com@18733996-620b-faea-2c97-0bf94f809d5c>
2014-11-11 16:38:26 +0000

    Simple Evaluation Modified - robot run in straigth line to nest

--- JBotDefensePatrol/src/fitnessfunctions/SimpleEvaluationFunction.java
+++ JBotDefensePatrol/src/fitnessfunctions/SimpleEvaluationFunction.java
@@ -8,9 +8,14 @@ import simulation.environment.RoundForageEnvironment;
 import simulation.robot.Robot;
 import simulation.util.Arguments;
 
+/* 
+ * Função para mover robot até ao ninho
+ */
+
 public class SimpleEvaluationFunction extends EvaluationFunction {
 	private Vector2d    nestPosition = new Vector2d(0, 0);
 	private double      NESTRADIUS;
+	private double lastDistanceToNest = 99999999;
 
 	public SimpleEvaluationFunction(Arguments args) {
 		super(args);
@@ -19,23 +24,28 @@ public class SimpleEvaluationFunction extends EvaluationFunction {
 	@Override
 	public void update(Simulator simulator) {			
 		int numberOfRobotsCloseToNest = 0;
-
+		
 		NESTRADIUS         = ((SimpleEnvironment)(simulator.getEnvironment())).getNestRadius();
 		Vector2d pos=null;
 		Vector2d coord = new Vector2d();
 		for(Robot r : simulator.getEnvironment().getRobots()){
-			coord.set(r.getPosition());
 			pos=r.getPosition();
-			double distanceToNest = coord.distanceTo(nestPosition);
-			if (distanceToNest < NESTRADIUS+1) {
-				numberOfRobotsCloseToNest++;
-			} 
+			if(lastDistanceToNest==99999999){
+				lastDistanceToNest=pos.distanceTo(nestPosition);
+			}
+			/* Dá recompensa de cada vez que se aproxima do ninho e dá castigo de cada vez que se afasta */
+			if(lastDistanceToNest>pos.distanceTo(nestPosition)){
+				fitness+=(lastDistanceToNest-pos.distanceTo(nestPosition))*100;
+			}else{
+				fitness-=(pos.distanceTo(nestPosition)-lastDistanceToNest)*100;
+			}
+			lastDistanceToNest = pos.distanceTo(nestPosition);
+			// Dá recompensa quando o robot está no meio
+			if(pos.distanceTo(nestPosition) <=0.01){
+				fitness+=2;
+			}
 		}
-		fitness += (double) numberOfRobotsCloseToNest * 0.1;
-		if(pos.distanceTo(nestPosition) <=0.01){
-			fitness+=1;
-			//simulator.getEnvironment().getRobots().get(0).stop();
-		}
+		
 	}
 
 }
